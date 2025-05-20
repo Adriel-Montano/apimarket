d6a44768a94107f4f42f7df78b76803d6c7e8b97
Adrielfloo <ML232940@alumno.udb.edu.sv>
2025-05-20 13:09:24 -0600

    Configurar API con rutas, controladores y conexión a la base de datos

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
-import express from 'express'
-import marketRoutes from './routes/market.routes.js'
+import express from 'express';
+import marketRoutes from './routes/market.routes.js';
+const app = express();
 
-const app=express()
-
-// Middleware para procesar JSON
 app.use(express.json());
-
-// Middleware para procesar datos de formularios (opcional, si usas x-www-form-urlencoded)
 app.use(express.urlencoded({ extended: true }));
-
-app.use(marketRoutes)
-
-app.use((req,res,next)=>{
+app.use(marketRoutes);
+app.use((req, res, next) => {
     res.status(404).json({
-        message:'Endpoint No encontrado'
-    })
-})
+        message: "Favor realizar pruebas en los siguientes endpoints:",
+        endpoints: [
+            "https://apimarket-production-6167.up.railway.app/usuarios",
+            "https://apimarket-production-6167.up.railway.app/productos"
+        ]
+    });
+});
 
 export default app;
